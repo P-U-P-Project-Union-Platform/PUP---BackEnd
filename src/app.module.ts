18684b8ddf778187268154d3f8b6a0a94f2691fc
Gri22ly <79156923+Junseung0526@users.noreply.github.com>
2025-07-15 17:06:39 +0900

    db setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+
+const entities = []; // 아직 엔티티가 없다면 비워두세요.
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DATABASE_TYPE', 'mysql') as any,
+        host: configService.get<string>('DATABASE_HOST')!,         // <-- Add '!' here
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME')!, // <-- Add '!' here
+        password: configService.get<string>('DATABASE_PASSWORD')!, // <-- Add '!' here
+        database: configService.get<string>('DATABASE_NAME')!,     // <-- Add '!' here
+        entities: entities,
+        synchronize: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
